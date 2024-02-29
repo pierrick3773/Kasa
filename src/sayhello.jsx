a81ae5c4a10438443bd38cc305d7cb069c05a163
pierrick3773 <pviarde@gmail.com>
2024-02-29 15:57:12 +0100

    supression test cours

--- src/sayhello.jsx
+++
@@ -1,9 +0,0 @@
-export function SayHelloAll() {
-  return (
-    <div style={{ border: "3px solid blue" }}>
-      <p>i'm SayHelloAll component</p>
-      <h1>hello</h1>
-      <p>salut react</p>
-    </div>
-  );
-}
