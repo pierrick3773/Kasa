c1e5ba543f2deb7689c157324c12033206c98eed
pierrick3773 <pviarde@gmail.com>
2024-02-29 17:17:17 +0100

    ajout footer et essai rooter

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,12 @@ import "./index.css";
 import { Header } from "./header.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Card } from "./Card.jsx";
+import { Footer } from "./Footer.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  // a priori obligé de faire cette manimp parceque le projet est sur reactrouter17
+  // et moi je suis sur le 18 ! mais la div root fou le bordel pour la mise en page du footer
   <BrowserRouter>
     <Header />
     <main>
@@ -20,7 +23,7 @@ root.render(
         <Card src="https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-5-1.jpg" />
       </section>
     </main>
-
+    <Footer />
     {/* <App /> */}
   </BrowserRouter>
 );
