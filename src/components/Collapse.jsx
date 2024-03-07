17c5a9898b2785fa931c2ff67afd1cfb62b23609
pierrick3773 <pviarde@gmail.com>
2024-03-07 10:24:58 +0100

    avancé code collapse

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,12 +1,16 @@
-import React from "react";
-export const Collapsible = () => {
+import React, { useState, useRef } from "react";
+
+export const Collapsible = (props) => {
+  const [open, setOPen] = useState(false);
+  const toggle = () => {
+    setOPen(!open);
+  };
+  const contentRef = useRef();
   return (
     <div>
-      <p>
-        Les annonces postées sur Kasa garantissent une fiabilité totale. Les
-        photos sont conformes aux logements, et toutes les informations sont
-        régulièrement vérifiées par nos équipes
-      </p>
+      <button>
+        {toggle}={props.label}
+      </button>
     </div>
   );
 };
--- src/pages/A-Propos.jsx
+++ src/pages/A-Propos.jsx
@@ -2,8 +2,36 @@ import { Collapsible } from "../components/Collapse.jsx";
 
 export function Apropos() {
   return (
-    <div>
-      <Collapsible />
+    <div className="a-propos">
+      <Collapsible label="Fiabilité" />
+      <p>
+        Les annonces postées sur Kasa garantissent une fiabilité totale. Les
+        photos sont conformes aux logements, et toutes les informations sont
+        régulièrement vérifiées par nos équipes
+      </p>
+
+      <Collapsible label="Respect" />
+      <p>
+        La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
+        comportement dicriminatoire ou de perturbation deu voisinage entraînera
+        une exclusion de notre plateforme
+      </p>
+
+      <Collapsible label="Service" />
+      <p>
+        Nos équipes se tiennent à votre disposition pour vous fournir une
+        expérience parfaite. N'hésitez pas à nous contacter si vous avez la
+        moindre question.
+      </p>
+      <Collapsible label="Sécurité" />
+      <p>
+        La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour
+        les voyageurs, chaque logement correspond aux critères de sécurité
+        établis par nos services. En laissant une note aussi bien à l'hôte qu'au
+        locataire, cela permet à nos équipes de vérifier que les standars sont
+        bien respectés. Nous organisons également des ateliers sur la sécurité
+        domestique pou nos hôtes.
+      </p>
     </div>
   );
 }
