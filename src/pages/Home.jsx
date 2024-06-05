0f7f9373c60e6e8c42a514a3590b0f5586cc2d67
Pierrick_Viarde <pviarde@gmail.com>
2024-06-05 10:17:31 +0200

    image banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ let title = "Chez vous,<wbr/> partout&nbsp;et&nbsp;ailleurs";
 export function Home() {
   return (
     <div>
-      <Banner title={title} image="src/assets/Image-source-1.svg" />
+      <Banner title={title} image="../../../public/Image-source-1.svg" />
       <Card annoncesData={annoncesData} />
     </div>
   );
