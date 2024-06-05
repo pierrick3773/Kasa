bd1f2429ce7092fe0e3addd228ecb8b91fd26bab
Pierrick_Viarde <pviarde@gmail.com>
2024-06-05 10:22:41 +0200

    banner img

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,11 @@
 import { Banner } from "../components/Banner";
 import { Card } from "../components/Card";
 import annoncesData from "../data/logement.json";
-import image from "../../../public/Image-source-1.svg"; 
-
 let title = "Chez vous,<wbr/> partout&nbsp;et&nbsp;ailleurs";
-
 export function Home() {
   return (
     <div>
-      <Banner title={title} image={image} /> 
+      <Banner title={title} image="../../../public/Image-source-1.svg" />
       <Card annoncesData={annoncesData} />
     </div>
   );
